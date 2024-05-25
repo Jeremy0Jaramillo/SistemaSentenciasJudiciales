f3a2764696a13f437eab8b7e5d99054cba8cee08
carolaljime21 <carolalvaradojime@gmail.com>
2024-05-25 16:51:54 -0500

    Revert "Borrar FormPage"
    This reverts commit bfb97cedd69774a220e40c085bfd1a05b7ec182f, reversing
    changes made to 0654973889a7bb2b04d1d7f8fc6638013aba4ce0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login/login-page.component';
 import { PrincipalPageComponent } from './pages/principal/principal-page.component';
 import { SentenciasPageComponent } from './pages/sentencias/sentencias-page.component.';
-import { AnalisisComponent } from './pages/analisis/analisis.component';
-import { Analisis2Component } from './pages/analisis2/analisis2.component';
-import { EvaluacionComponent } from './pages/evaluacion/evaluacion.component';
+import { FormPageComponent } from './pages/form-page/form-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'principal', component: PrincipalPageComponent },
   { path: 'nueva-sentencia', component: SentenciasPageComponent },
-  { path: 'analisis', component: AnalisisComponent },
-  { path: 'analisis2', component: Analisis2Component },
-  { path: 'evaluacion', component: EvaluacionComponent }
+  { path: 'form-page', component: FormPageComponent }
   // Define other routes here
 ];
 
