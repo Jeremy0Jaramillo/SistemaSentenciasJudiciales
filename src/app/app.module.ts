a8658ccc24cd08cd9136d4481ed3ae3cb72ed732
carolaljime21 <carolalvaradojime@gmail.com>
2024-05-25 19:38:42 -0500

    Problemas Solucionados (Falta estilos)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,11 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HeaderComponent } from '../app/components/header/header.component';
 import { SentenciasPageComponent } from '../app/pages/sentencias/sentencias-page.component.';
 import { PrincipalPageComponent } from '../app/pages/principal/principal-page.component';
-<<<<<<< HEAD
-import { FormPageComponent } from './pages/form-page/form-page.component';
-import { CajaTextoComponent } from './components/header/caja-texto/caja-texto.component';
-=======
 import { AnalisisComponent } from './pages/analisis/analisis.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EvaluacionComponent } from './pages/evaluacion/evaluacion.component';
 import { Analisis2Component } from './pages/analisis2/analisis2.component';
->>>>>>> a87354a1a276d92c1b39214c4c4cf7257622d3e8
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -28,14 +24,9 @@ import { Analisis2Component } from './pages/analisis2/analisis2.component';
     HeaderComponent,
     SentenciasPageComponent,
     PrincipalPageComponent,
-<<<<<<< HEAD
-    FormPageComponent,
-    CajaTextoComponent
-=======
     AnalisisComponent,
     Analisis2Component,
     EvaluacionComponent
->>>>>>> a87354a1a276d92c1b39214c4c4cf7257622d3e8
   ],
   imports: [
     BrowserModule,
@@ -44,7 +35,8 @@ import { Analisis2Component } from './pages/analisis2/analisis2.component';
     AngularFireAuthModule,
     ReactiveFormsModule,
     FormsModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
